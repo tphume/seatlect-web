c855516de74221d21ba781705ce36639132a6754
Traphume <traphumedev@gmail.com>
2020-12-03 16:52:36 +0700

    Setup material ui for ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import theme from 'src/theme.js';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // When render on client remove server-side injected css
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    // Wrap App in Material server side stylesheet
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
