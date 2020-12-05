61728ef74f0148567e4d3caa927ca40c0f79af39
Traphume <traphumedev@gmail.com>
2020-12-05 16:36:08 +0700

    Customized theme colors and font

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,29 @@
 import { createMuiTheme } from '@material-ui/core';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#5d55b4'
+    },
+    secondary: {
+      main: '#f2b705'
+    },
+    error: {
+      main: '#f24b4b'
+    },
+    success: {
+      main: '#65bf7c'
+    }
+  },
+  typography: {
+    fontFamily: 'DM Sans'
+  },
+  props: {
+    MuiAppBar: {
+      color: 'secondary'
+    }
+  }
+});
 
 export default theme;
