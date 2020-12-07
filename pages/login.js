b969f4483375efc6c6935c043a53cd48572c5cc2
Traphume <traphumedev@gmail.com>
2020-12-07 22:19:37 +0700

    Added basic section to login page

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  root: {
+    height: '100vh'
+  }
+});
+
 export default function Login() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hi, you are at login</h1>
-    </div>
+    <Grid container component="main" spacing={0} className={classes.root}>
+      <Grid item component="div" sm={6}></Grid>
+      <Grid item component="section" sm={6}></Grid>
+    </Grid>
   );
 }
