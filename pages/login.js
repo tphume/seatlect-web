827572ced254b243abc8fd37f005aaf9ccd78e7c
Traphume <traphumedev@gmail.com>
2020-12-07 23:38:03 +0700

    Added empty username and password input field

--- pages/login.js
+++ pages/login.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
   root: {
     height: `100vh`
   },
   loginRoot: {
-    padding: `10vh 5% 10vh 5%`
+    padding: `10vh 10% 10vh 10%`
   },
   loginSubtitle: {
     color: `#BDBDBD`
@@ -24,12 +25,35 @@ export default function Login() {
     <Grid container component="main" spacing={0} className={classes.root}>
       <Grid item component="div" sm={6}></Grid>
       <Grid item component="section" sm={6} className={classes.loginRoot}>
-        <Typography component="h1" variant="h3">
-          Seatlect
-        </Typography>
-        <Typography component="h3" variant="h6" classes={{ root: classes.loginSubtitle }}>
-          Please login to continue
-        </Typography>
+        <Box marginBottom={`2rem`}>
+          <Typography component="h1" variant="h3">
+            Seatlect
+          </Typography>
+          <Typography component="h3" variant="h6" classes={{ root: classes.loginSubtitle }}>
+            Please login to continue
+          </Typography>
+        </Box>
+        <form autoComplete="off">
+          <TextField
+            required
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            label="Username"
+            placeholder="Jiaroach"
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            required
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            label="Password"
+            placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+            InputLabelProps={{ shrink: true }}
+            type="password"
+          />
+        </form>
       </Grid>
     </Grid>
   );
