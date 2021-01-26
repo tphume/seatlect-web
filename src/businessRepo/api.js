d6e43480991241f765e5450eb7243d711cdfcea9
Traphume <traphumedev@gmail.com>
2021-01-26 23:14:37 +0700

    Added mock getBusiness

--- src/businessRepo/api.js
+++ src/businessRepo/api.js
@@ -18,7 +18,27 @@ class BusinessRepo {
   }
 }
 
-class BusinessMockRepo { }
+class BusinessMockRepo {
+  async getBusiness() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Uncomment the following if you want to test error
+    // throw 'Fake error';
+
+    return {
+      businessName: 'JiaRoast',
+      type: 'Bar',
+      tags: ['Korean', 'Alcohol'],
+      description:
+        'Korean style bar with an emphasis on roast side dishes. Made by certified chef Jiaroach',
+      location: { latitude: '', longitude: '' },
+      address: '2FL. Groove Central World, Pathum Wan District, Bangkok 10330',
+      displayImage: 'https://i.imgur.com/g17EY2i.jpg',
+      images: ['https://i.imgur.com/g17EY2i.jpg', 'https://i.imgur.com/RjFgQSZ.jpeg'],
+      policy: { minAge: 18 }
+    };
+  }
+}
 
 function getBusinessRepo() { }
 
