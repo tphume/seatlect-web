c78d262386d8b8abd4ecc9d08dacab747c9011ab
Traphume <traphumedev@gmail.com>
2021-02-04 14:25:22 +0700

    Added RequestRepo

---
+++ src/requestRepo/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+class RequestRepo {
+	constructor({ url, endpoint = '/request', id }) {
+		// Must pass in url and id
+		this.url = url;
+		this.endpoint = `${endpoint}/${id}`;
+	}
+
+	async createRequest(args) {
+		const { businessName, type, tags, description, location, address, policy } = args;
+		// TODO: Check arguments
+
+		try {
+			const response = await axios.post(this.url + endpoint, args);
+			return response;
+		} catch (e) {
+			// TODO add better error handling
+			throw 'Network error';
+		}
+	}
+}
+
+class RequestMockRepo {
+	async makeRequest(args) {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+
+		// Uncomment the following if you want to test error
+		// throw 'Fake error';
+	}
+}
+
+function getRequestRepo({ env, id }) {
+	if (env === 'development') {
+		return new RequestMockRepo();
+	}
+
+	return new RequestRepo({ env, id });
+}
+
+export { RequestRepo, RequestMockRepo, getRequestRepo };
---
+++ src/requestRepo/index.js
@@ -0,0 +1,1 @@
+export { RequestRepo, RequestMockRepo, getRequestRepo } from './api.js';
