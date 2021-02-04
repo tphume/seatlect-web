e0d46e46f71d952022dcd74adbfa8bc81cacba98
Traphume <traphumedev@gmail.com>
2021-02-04 11:30:02 +0700

    Added displayImage card

--- pages/home/[id].js
+++ pages/home/[id].js
@@ -10,6 +10,13 @@ import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -32,6 +39,10 @@ const useStyles = makeStyles((theme) => ({
   mapContainer: {
     width: `100%`,
     height: `300px`
+  },
+  displayCard: {
+    maxWidth: `375px`,
+    margin: `auto`
   }
 }));
 
@@ -115,6 +126,26 @@ export default function Home({ env, url, initial }) {
               ></GoogleMapReact>
             </div>
           </form>
+        </Grid>
+        <Grid item component="div" sm={6}>
+          <Card className={classes.displayCard} variant="outlined">
+            <CardMedia image={business.displayImage} component="img" height="225" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                This will be the image that is used to represent your Business in the mobile
+                application. It is the first image the customer will see.
+              </Typography>
+            </CardContent>
+            <CardActionArea />
+            <CardActions>
+              <Button size="small" color="primary">
+                Edit
+              </Button>
+            </CardActions>
+          </Card>
         </Grid>
       </Grid>
     </Layout>
