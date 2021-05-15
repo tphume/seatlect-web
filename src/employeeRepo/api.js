d13dc6ba0ca3aa6ef56b7a6e397196b55dec0965
Traphume <traphumedev@gmail.com>
2021-05-15 17:48:30 +0700

    Added getEmployee to EmployeeRepo

--- src/employeeRepo/api.js
+++ src/employeeRepo/api.js
@@ -6,9 +6,31 @@ class EmployeeRepo {
 		this.url = url;
 		this.endpoint = `${endpoint}/${id}`;
 	}
+
+	async getEmployee() {
+		try {
+			const response = await axios.get(this.url + this.endpoint);
+			return response.data.employees;
+		} catch (e) {
+			// TODO add better error handling
+			throw 'Network error';
+		}
+	}
 }
 
 class EmployeeMockRepo {
+	async getEmployee() {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+
+		// Uncomment the following if you want to test error
+		// throw 'Fake error';
+
+		return [
+			{ username: "Wintur", password: "ExamplePassword" },
+			{ username: "Summur", password: "ExamplePassword" },
+			{ username: "Chodata", password: "WeakPassword" }
+		];
+	}
 }
 
 function getEmployeeRepo({ url, env, id }) {
